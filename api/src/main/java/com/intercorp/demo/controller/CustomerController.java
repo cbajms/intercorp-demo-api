dd9758b297346f78b7a288bb650655a259066b7e
cbajms <cbajms@gmail.com>
2021-06-07 16:46:36 -0300

    fix: application.yml

--- api/src/main/java/com/intercorp/demo/controller/CustomerController.java
+++ api/src/main/java/com/intercorp/demo/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.intercorp.demo.controller;
 import com.intercorp.demo.model.KpiResponseBody;
 import com.intercorp.demo.service.CustomerService;
 import com.intercorp.demo.model.Customer;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,16 +26,19 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.getById(customerId));
     }
 
+    @ApiOperation(value = "Crear cliente: nombre, apellido, edad, fecha_nacimiento")
     @PostMapping()
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         return ResponseEntity.ok(customerService.create(customer));
     }
 
+    @ApiOperation(value = "Crear cliente masivo. Recibe un array de Clientes")
     @PostMapping(value = "/batch")
     public ResponseEntity<List<Customer>> createCustomer(@RequestBody List<Customer> customers) {
         return ResponseEntity.ok(customerService.batchCreate(customers));
     }
 
+    @ApiOperation(value = "Retorna KpiResponseBody: promedio de edades de todos los clientes, Desviación estándar entre las edades de todos los clientes")
     @GetMapping(value = "/kpideclientes", produces = "application/json")
     public ResponseEntity<KpiResponseBody> getKPI() {
         return ResponseEntity.ok(customerService.getKpiResponse());
--- api/src/main/java/com/intercorp/demo/controller/EmployeeController.java
+++ api/src/main/java/com/intercorp/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.intercorp.demo.controller;
 
 import com.intercorp.demo.model.Employee;
 import com.intercorp.demo.service.EmployeeService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,18 +15,21 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @ApiOperation(value = "Recupera todos los empleados de la tabla ejemplo de oracle HR.EMPLOYEES")
     @GetMapping(produces = "application/json")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAll());
     }
 
+    @ApiOperation(value = "Recupera un empleado por su id, de la tabla ejemplo de oracle HR.EMPLOYEES")
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId) {
         return ResponseEntity.ok(employeeService.getById(employeeId));
     }
 
+    @ApiOperation(value = "Crea un nuevo empleado en la tabla ejemplo de oracle HR.EMPLOYEES")
     @PostMapping()
-    public ResponseEntity<Employee> createCustomer(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         return ResponseEntity.ok(employeeService.create(employee));
     }
 }
